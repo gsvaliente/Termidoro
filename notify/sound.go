ce8cd88a2f81a529e24141c8e8eba9680432e719
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2026-01-12 21:35:47 +0100

    Basic implementation done

---
+++ notify/sound.go
@@ -0,0 +1,11 @@
+package notify
+
+import "github.com/gen2brain/beeep"
+
+func PlayCompletionSound() error {
+	return beeep.Notify("Pomodoro Complete", "Time is up!", "")
+}
+
+func PlayAlert(title, message string) error {
+	return beeep.Alert(title, message, "")
+}
---
+++ timer/engine.go
@@ -0,0 +1,94 @@
+package timer
+
+import (
+	"fmt"
+	"time"
+)
+
+type Session struct {
+	Duration    time.Duration
+	StartTime   time.Time
+	EndTime     time.Time
+	Completed   bool
+	WasCancelled bool
+}
+
+type Engine struct {
+	Sessions   []Session
+	TotalTime  time.Duration
+}
+
+func NewEngine() *Engine {
+	return &Engine{
+		Sessions:  []Session{},
+		TotalTime: 0,
+	}
+}
+
+func (e *Engine) AddSession(duration time.Duration) {
+	session := Session{
+		Duration:  duration,
+		StartTime: time.Now(),
+		EndTime:   time.Now().Add(duration),
+		Completed: false,
+	}
+	e.Sessions = append(e.Sessions, session)
+}
+
+func (e *Engine) CompleteSession(index int) {
+	if index >= 0 && index < len(e.Sessions) {
+		e.Sessions[index].Completed = true
+		e.TotalTime += e.Sessions[index].Duration
+	}
+}
+
+func (e *Engine) CancelSession(index int) {
+	if index >= 0 && index < len(e.Sessions) {
+		e.Sessions[index].WasCancelled = true
+	}
+}
+
+func (e *Engine) GetSessionDuration(index int) time.Duration {
+	if index >= 0 && index < len(e.Sessions) {
+		return e.Sessions[index].Duration
+	}
+	return 0
+}
+
+func (e *Engine) GetSessionStartTime(index int) time.Time {
+	if index >= 0 && index < len(e.Sessions) {
+		return e.Sessions[index].StartTime
+	}
+	return time.Time{}
+}
+
+func (e *Engine) SessionCount() int {
+	return len(e.Sessions)
+}
+
+func (e *Engine) CompletedSessionCount() int {
+	count := 0
+	for _, s := range e.Sessions {
+		if s.Completed {
+			count++
+		}
+	}
+	return count
+}
+
+func FormatDuration(d time.Duration) string {
+	minutes := int(d.Minutes())
+	seconds := int(d.Seconds()) % 60
+	return fmt.Sprintf("%dm %ds", minutes, seconds)
+}
+
+func FormatDurationShort(d time.Duration) string {
+	minutes := int(d.Minutes())
+	seconds := int(d.Seconds()) % 60
+	return fmt.Sprintf("%dm %02ds", minutes, seconds)
+}
+
+func FormatDurationMinutes(d time.Duration) string {
+	minutes := int(d.Minutes())
+	return fmt.Sprintf("%dm", minutes)
+}
